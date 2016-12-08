3c9fb39c9df3c21ad55ba4f33aa38cb697ba766f
Michael Aubrey <mcaubrey511@gmail.com>
2016-12-08 02:21:47 +0000

    added palette to preload fonts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 /*global $*/
 
 $("document").ready(function(){
+    
     $(".bigger").click(function(){
         var fontSize = parseInt($(".input-area").css("font-size"));
         fontSize += 10;
@@ -17,15 +18,16 @@ $("document").ready(function(){
     
     $(".cute").click(function(){
         $(".input-area").css("font-family", "Cute Japanese");
+
     });
-    
-    
     
     $(".stroke").click(function(){
         $(".input-area").css("font-family", "KSO");
+
     });
     
     $(".normal").click(function(){
         $(".input-area").css("font-family", '"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "Roboto", sans-serif');
+
     })
 });
