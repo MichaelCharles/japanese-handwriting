a8701813e7aff012164d1483ef9ffd9eb748a731
Michael Aubrey <mcaubrey511@gmail.com>
2016-12-08 07:14:13 +0000

    autofocus

--- js/index.js
+++ js/index.js
@@ -30,9 +30,12 @@ $("document").ready(function() {
         $(".input-area").css("font-family", '"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "Roboto", sans-serif');
 
     })
-    
-    $(".font-button").click(function(){
+
+    $(".font-button").click(function() {
         $(".button").removeClass("selected-button");
         $(this).addClass("selected-button");
     });
+
+    $(".input-area").focus();
+    $(".input-area").html("日本語で書いて！")
 });
