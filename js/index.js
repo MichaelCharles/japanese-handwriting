baa13a687caba684ef9124e3db6418cbec3d7139
Michael Aubrey <mcaubrey511@gmail.com>
2016-12-08 03:34:56 +0000

    added meta data

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
 /*global $*/
 
-$("document").ready(function(){
-    
-    $(".bigger").click(function(){
+$("document").ready(function() {
+
+    $(".bigger").click(function() {
         var fontSize = parseInt($(".input-area").css("font-size"));
         fontSize += 10;
         $(".input-area").css("font-size", fontSize);
     });
-    
-    
-    $(".smaller").click(function(){
+
+
+    $(".smaller").click(function() {
         var fontSize = parseInt($(".input-area").css("font-size"));
         fontSize -= 10;
         $(".input-area").css("font-size", fontSize);
     });
-    
-    
-    $(".cute").click(function(){
+
+
+    $(".cute").click(function() {
         $(".input-area").css("font-family", "Cute Japanese");
 
     });
-    
-    $(".stroke").click(function(){
+
+    $(".stroke").click(function() {
         $(".input-area").css("font-family", "KSO");
 
     });
-    
-    $(".normal").click(function(){
+
+    $(".normal").click(function() {
         $(".input-area").css("font-family", '"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "Roboto", sans-serif');
 
     })
