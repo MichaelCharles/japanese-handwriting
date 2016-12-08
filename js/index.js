1f2deaa88304c0143ad045eacac83a26424c384d
Michael Aubrey <mcaubrey511@gmail.com>
2016-12-08 01:57:52 +0000

    minor text fixes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*global $*/
+
 $("document").ready(function(){
     $(".bigger").click(function(){
         var fontSize = parseInt($(".input-area").css("font-size"));
@@ -22,4 +24,8 @@ $("document").ready(function(){
     $(".stroke").click(function(){
         $(".input-area").css("font-family", "KSO");
     });
+    
+    $(".normal").click(function(){
+        $(".input-area").css("font-family", '"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "Roboto", sans-serif');
+    })
 });
